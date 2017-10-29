decc7ca77c96aa76def9ffa25ec50f338348fd23
unknown <l3754902@GMAIL.COM>
2017-10-29 22:27:13 +0800

    all done

--- AprioriAlgo/FP_growth.py
+++ AprioriAlgo/FP_growth.py
@@ -1,6 +1,6 @@
 from file_utills import file_tool
 from parameters import file_name, min_sup, file_name_IBM, file_name_IBM1
-from collections import Counter
+import time
 
 class Node(object):
 
@@ -27,6 +27,18 @@ class Node(object):
                 child.find(item)
         return None
 
+    def filter(self, limit, label, current_pattern, F_dict):  # [frozenset(),frozenset(),]
+
+        if self.child:
+            for item in self.child.values():
+                if item.count < limit:
+                    continue
+                else:
+                    F_dict[frozenset([label, item.label])] = item.count
+                    current_pattern.append(item.label)  # [node.label, item.label]
+                    F_dict[frozenset(current_pattern)] = item.count
+                    item.filter(limit, label, current_pattern, F_dict)
+
 
 def sortListByfrequency(item_list, freq_dict):
 
@@ -34,33 +46,25 @@ def sortListByfrequency(item_list, freq_dict):
     return [x for _, x in sorted(zip(freq_list, item_list), reverse=True)]
 
 
-def countSychroz(node, count, dic):
-    if node.label != 'root':
-        dic[node] = count
-        if node.parent:
-            countSychroz(node.parent, count, dic)
-
 def sub_path(node, count):
 
     list = []
     if node:
         while node.label != 'root':
-            node.count = count
-            list.append(tuple([node.label, node.count]))
+            list.append(tuple([node.label, count]))
             node = node.parent
 
     return list[::-1]
 
 
-
 def build_FP(root, path):
-    current_Node = root
 
+    current_Node = root
     for item in path:
         stop_Node = current_Node.find(item[0])
         '''ever show'''
         if stop_Node:
-            stop_Node.inc(1)
+            stop_Node.inc(stop_Node.count)
             next_Node = stop_Node
 
         else:
@@ -71,30 +75,56 @@ def build_FP(root, path):
         current_Node = next_Node
 
 
-def main():
+def obtain_Pattern(root, minsup, label):
 
-    f = file_tool('data/Te.csv', False)
+    F_dict = {}
+    children = root.child.values()
+    if children:
+        for node in children:  # root下的父點
+            if node.count < minsup:
+                continue
+            else:
+                if frozenset([label, node.label]) in F_dict:
+                    F_dict[frozenset([label, node.label])] = F_dict[frozenset([label, node.label])] + node.count
+                else:
+                    F_dict[frozenset([label, node.label])] = node.count
+
+                node.filter(min_sup, label, [label, node.label], F_dict)
+
+    return F_dict
+
+
+def main():
+    global file_len
+
+    f = file_tool(file_name_IBM1, False)
+    file_len = len(f.csv_file)
+    min_sup_n = min_sup * file_len
+
     transaction = f.dict
     init_Dict = f.item_id_dict()
     NodeTable = {}
     label_Node = {}
 
+    tStart = time.time()
+    ''' build frequency Table '''
     # {word} --> frequency(int)
     for i in list(init_Dict):  # item : freq
         init_Dict[i] = len(init_Dict[i])
 
+    ''' sort frequency Table '''
     # sort dict by frequency , output a list
     freq_List = sorted(init_Dict.items(), key=lambda d: d[1], reverse=False)
 
+    ''' sort transaction  '''
     # tid --> [ x,y,z ]
-    # sort in transaction
     for key, value in transaction.items():
         transaction[key] = sortListByfrequency(transaction[key], init_Dict)
 
+    ''' build main FP-Tree '''
     rootNode = Node('root', None, None)
     init_Dict['root'] = 0
 
-    # build Tree
     for item_list in transaction.values():
         current_Node = rootNode
         path = []
@@ -121,35 +151,35 @@ def main():
             NodeTable[next_Node.label].add(tuple(path))
             label_Node[next_Node.label].add(next_Node)
 
-    '''NodeTable store pattern path
-    as Node --> [] path_List '''
+    #rootNode.disp()
 
-    for key, pat_list in NodeTable.items():
-        print('item:', key)
-        print("*****")
-        print(pat_list)
-        for i in pat_list:
-            for x in i:
-                print('pattern: ', x.label, x.count)
-            print(".....")
-        print("-------------------")
-    rootNode.disp()
+    all_pattern = []
 
     ''' Extract suffix patterns of paths to the item i
  Construct its Conditional Pattern Base '''
-
     for i in freq_List:  # label --> frequency
-        current_NodeSet = label_Node[i[0]]  # = contains all sub-Node
-        print('\n', i[0])
 
+        label = i[0]
+        current_NodeSet = label_Node[label]  # = contains all sub-Node
         root = Node('null', None, None)
+
         for node in current_NodeSet:
             path = sub_path(node.parent, node.count)
-            print(node.label, path)
+            #print(node.label, path)
             build_FP(root, path)
 
-        root.disp()  # show sub-tree
+        # root.disp()  # show sub-tree
+
+        final_dict = obtain_Pattern(root, min_sup_n, label)
+
+        for x, j in final_dict.items():
+            print(x, j)
+
+    tEnd = time.time()
+    print('%fs' % (tEnd - tStart))
+
 
 if __name__ == '__main__':
+
     main()
 
